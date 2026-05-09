3bd8566eb13b970bd87861a51a5c62b0f5cdb38a
figamore <90107339+figamore@users.noreply.github.com>
2026-05-09 09:31:37 -0400

    Auto-generate icons on build to reduce bundle size slightly

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import { fileURLToPath } from 'url'
 
 export default defineConfig(({ mode }) => ({
+  resolve: {
+    alias: {
+      'lucide-react': fileURLToPath(new URL('./src/icons.tsx', import.meta.url)),
+    },
+  },
   base: mode === 'demo' ? '/FigUI/' : '/',
   plugins: [
     react(),
